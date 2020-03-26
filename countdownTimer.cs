6f92407ba983c73e5f2921820f32af8986546473
tatumvt <62714286+tatumvt@users.noreply.github.com>
2020-03-26 17:50:22 +0100

    Add files via upload

---
+++ countdownTimer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class countdownTimer : MonoBehaviour
+{
+    public int levelToLoad;
+    float timer = 150f;
+    private Text timerSeconds;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerSeconds = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerSeconds.text = timer.ToString("f0");
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
